50b20d5168bc01d568e223ac8332b60f6c437f23
Asgyr <asgyrdjons@gmail.com>
2020-07-05 22:10:05 +0300

    add deckstop  slick list

---
+++ src/js/slaider.js
@@ -0,0 +1,16 @@
+$('.carousel__inner').slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }
+    ]
+});
