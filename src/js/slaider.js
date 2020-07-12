b0682ec730f50d771bc5bd05548abf261b52bfd0
Asgyr <asgyrdjons@gmail.com>
2020-07-12 21:22:46 +0300

    decstop ful

--- src/js/slaider.js
+++ src/js/slaider.js
@@ -1,16 +1,33 @@
-$('.carousel__inner').slick({
-    speed: 1200,
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
             }
+        ]
+    });
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 980) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
         }
-    ]
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
